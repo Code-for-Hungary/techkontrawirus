eb5f441d6fb3f34b3cf5eb4fcf6609eb9541a12b
Attila Juhász <juhasz.attila@k-monitor.hu>
2020-03-26 12:05:52 +0100

    Update 400.blade.php

--- resources/views/errors/400.blade.php
+++ resources/views/errors/400.blade.php
@@ -1,6 +1,6 @@
 @extends('base')
 
-@section('title', 'Tech Kontra Wirus | Błąd danych')
+@section('title', 'Tech Kontra Vírus | Adathiba')
 
 @section('styles')
 @endsection
@@ -9,10 +9,10 @@
     <div class="container">
         <div class="row h-100 align-items-center">
             <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 py-5 text-center">
-                <h1>Nieprawidłowe dane</h1>
+                <h1>Érvénytelen adatok</h1>
                 <h4>
-                    Nasz system otrzymał nieprawidłowe dane. Jeśli problem się powtórzy, powiadom nas pisząc na
-                    biuro@epf.org.pl.
+                    Rendszerünk érvénytelen adatokat kapott. Ha a probléma továbbra is fennáll, írd meg nekünk erre a címre:
+                    info@k-monitor.hu.
                 </h4>
             </div>
         </div>
