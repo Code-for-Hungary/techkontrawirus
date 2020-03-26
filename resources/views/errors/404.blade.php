0171c061fc6f13540fceeaec0a3863c2b6065a0b
Attila Juhász <juhasz.attila@k-monitor.hu>
2020-03-26 12:06:57 +0100

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,6 @@
 @extends('base')
 
-@section('title', 'Tech Kontra Wirus | Nie znaleziono strony')
+@section('title', 'Tech Kontra Vírus | Az oldal nem található')
 
 @section('styles')
 @endsection
@@ -9,8 +9,8 @@
     <div class="container">
         <div class="row h-100 align-items-center">
             <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 py-5 text-center">
-                <h1>Nie znaleziono podanej strony</h1>
-                <h4>Sprawdź czy wpisałeś właściwy adres.</h4>
+                <h1>Az oldal nem található</h1>
+                <h4>Ellenőrizd, hogy helyes címet adtál-e meg.</h4>
             </div>
         </div>
     </div>
